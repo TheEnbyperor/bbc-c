07e01ce27f1e0ea96674b6aa4290ef04120d541a
Banjam Misell <beninjam174@gmail.com>
2017-05-23 20:07:31 +0100

    Interpreter output supporting routines

--- bbcc/asm.py
+++ bbcc/asm.py
@@ -5,108 +5,11 @@ class ASM:
     ret = 0x87
 
     def __init__(self):
-        self.asm = """NEW
-10 FOR opt%=0 TO 2 STEP 2
-20 P%=&E00
-30 [
-40 OPT opt%
-50 .mul LDA #0
-60 STA &""" + self._to_hex(self.result-2) + """
-70 STA &""" + self._to_hex(self.result-3) + """
-80 LDX #&10 
-90 .mul1 LSR &""" + self._to_hex(self.num1-1) + """ 
-100 ROR &""" + self._to_hex(self.num1) + """
-110 BCC mul2 
-120 LDA &""" + self._to_hex(self.result-2) + """
-130 CLC
-140 ADC &""" + self._to_hex(self.num2) + """
-150 STA &""" + self._to_hex(self.result-2) + """
-160 LDA &""" + self._to_hex(self.result-3) + """
-170 ADC &""" + self._to_hex(self.num2-1) + """
-180 .mul2 ROR A 
-190 STA &""" + self._to_hex(self.result-3) + """ 
-200 ROR &""" + self._to_hex(self.result-2) + """
-210 ROR &""" + self._to_hex(self.result-1) + """
-220 ROR &""" + self._to_hex(self.result) + """
-230 DEX
-240 BNE mul1
-245 JSR movres
-250 RTS
-260 .div LDA #0
-270 STA &""" + self._to_hex(self.result) + """
-280 STA &""" + self._to_hex(self.result-1) + """
-290 LDX #16
-300 .div1 ASL &""" + self._to_hex(self.num1) + """
-310 ROL &""" + self._to_hex(self.num1-1) + """ 
-320 ROL &""" + self._to_hex(self.result) + """
-330 ROL &""" + self._to_hex(self.result-1) + """
-340 LDA &""" + self._to_hex(self.result) + """
-350 SEC
-360 SBC &""" + self._to_hex(self.num2) + """
-370 TAY
-380 LDA &""" + self._to_hex(self.result-1) + """
-390 SBC &""" + self._to_hex(self.num2-1) + """
-400 BCC div2
-410 STA &""" + self._to_hex(self.result-1) + """
-420 STY &""" + self._to_hex(self.result) + """
-430 INC &""" + self._to_hex(self.num1) + """
-440 .div2 DEX
-450 BNE div1
-560 RTS
-570 .add CLC
-580 LDA &""" + self._to_hex(self.num1) + """
-590 ADC &""" + self._to_hex(self.num2) + """
-600 STA &""" + self._to_hex(self.result) + """
-610 LDA &""" + self._to_hex(self.num1-1) + """
-620 ADC &""" + self._to_hex(self.num2-1) + """
-630 STA &""" + self._to_hex(self.result-1) + """
-635 JSR movres
-640 RTS
-650 .sub SEC
-660 LDA &""" + self._to_hex(self.num1) + """
-670 SBC &""" + self._to_hex(self.num2) + """
-680 STA &""" + self._to_hex(self.result) + """
-690 LDA &""" + self._to_hex(self.num1-1) + """
-700 SBC &""" + self._to_hex(self.num2-1) + """
-710 STA &""" + self._to_hex(self.result-1) + """
-715 JSR movres
-720 RTS
-740 .movnum LDA &""" + self._to_hex(self.num2) + """
-750 STA &""" + self._to_hex(self.num1) + """
-760 LDA &""" + self._to_hex(self.num2-1) + """
-770 STA &""" + self._to_hex(self.num1-1) + """
-780 RTS
-790 .movres LDA &""" + self._to_hex(self.result) + """
-800 STA &""" + self._to_hex(self.num2) + """
-810 LDA &""" + self._to_hex(self.result-1) + """
-820 STA &""" + self._to_hex(self.num2-1) + """
-830 RTS
-840 .neg LDA &""" + self._to_hex(self.num2) + """
-850 EOR #&FF
-860 STA &""" + self._to_hex(self.num2) + """
-870 LDA &""" + self._to_hex(self.num2-1) + """
-880 EOR #&FF
-890 STA &""" + self._to_hex(self.num2-1) + """
-900 LDA #0
-910 STA &""" + self._to_hex(self.num1-1) + """
-900 LDA #1
-910 STA &""" + self._to_hex(self.num1) + """
-920 JSR add
-930 JSR movres
-940 RTS
-950 .reset LDA #0
-960 STA &""" + self._to_hex(self.result) + """
-970 STA &""" + self._to_hex(self.result-1) + """
-980 STA &""" + self._to_hex(self.num1) + """
-990 STA &""" + self._to_hex(self.num1-1) + """
-1000 STA &""" + self._to_hex(self.num2) + """
-1010 STA &""" + self._to_hex(self.num2-1) + """
-1020 RTS
-"""
-        self.line = 1030
+        self.asm = """NEW"""
+        self.line = 10
 
     @staticmethod
-    def _to_hex(num):
+    def to_hex(num):
         return ('%02x' % num).upper()
 
     def add_inst(self, isnt, op="", label=""):
@@ -120,12 +23,6 @@ class ASM:
         self.line += 10
 
     def get_asm(self):
-        self.add_inst("]")
-        self.add_inst("NEXT", "opt%")
-        self.add_inst("CALL", "main")
-        self.add_inst("RL=&" + self._to_hex(self.ret))
-        self.add_inst("RH=&" + self._to_hex(self.ret-1))
-        self.add_inst("PRINT", "~?RH,~?RL")
         asm = self.asm
         asm += "RUN\n"
         return asm
--- bbcc/interpreter.py
+++ bbcc/interpreter.py
@@ -8,6 +8,273 @@ class Interpreter(ast.NodeVisitor):
     def __init__(self, asm):
         self.asm = asm
 
+    def visit_Root(self, node):
+        self.asm.add_inst("FOR", "opt%=0 TO 2 STEP 2")
+        self.asm.add_inst("P%=&E00")
+        self.asm.add_inst("[")
+        self.asm.add_inst("OPT", "opt%")
+        self.asm.add_inst("LDA", "#0", "mul")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 2))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 3))
+        self.asm.add_inst("LDX", "#&10")
+        self.asm.add_inst("LSR", "&" + self.asm.to_hex(self.asm.num1 - 1), "mul1")
+        self.asm.add_inst("ROR", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("BCC", "mul2") 
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.result - 2))
+        self.asm.add_inst("CLC")
+        self.asm.add_inst("ADC", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 2))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.result - 3))
+        self.asm.add_inst("ADC", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("ROR", "A", "mul2")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 3))
+        self.asm.add_inst("ROR", "&" + self.asm.to_hex(self.asm.result - 2))
+        self.asm.add_inst("ROR", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("ROR", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("DEX")
+        self.asm.add_inst("BNE", "mul1")
+        self.asm.add_inst("JSR" "movres")
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("LDA", "#0", "div")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("LDX", "#16")
+        self.asm.add_inst("ASL", "&" + self.asm.to_hex(self.asm.num1), "div1")
+        self.asm.add_inst("ROL", "&" + self.asm.to_hex(self.asm.num1 - 1))
+        self.asm.add_inst("ROL", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("ROL", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("SEC")
+        self.asm.add_inst("SBC", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("TAY")
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("SBC", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("BCC", "div2")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("STY", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("INC", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("DEX", label="div2")
+        self.asm.add_inst("BNE", "div1")
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("CLC", label="add")
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("ADC", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num1 - 1))
+        self.asm.add_inst("ADC", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("JSR", "movres")
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("SEC", label="sub")
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("SBC", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num1 - 1))
+        self.asm.add_inst("SBC", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("JSR", "movres")
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num2), "movnum")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num1 - 1))
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.result), "movres")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num2), "neg")
+        self.asm.add_inst("EOR", "#&FF")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("LDA", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("EOR", "#&FF")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("LDA", "#0")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num1 - 1))
+        self.asm.add_inst("LDA", "#1")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("JSR", "add")
+        self.asm.add_inst("JSR", "movres")
+        self.asm.add_inst("RTS")
+        self.asm.add_inst("LDA", "#0", "reset")
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.result - 1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num1 - 1))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num2))
+        self.asm.add_inst("STA", "&" + self.asm.to_hex(self.asm.num2 - 1))
+        self.asm.add_inst("RTS")
+        for n in node.nodes:
+            self.visit(n)
+        self.asm.add_inst("]")
+        self.asm.add_inst("NEXT", "opt%")
+        self.asm.add_inst("CALL", "main")
+        self.asm.add_inst("RL=&" + self.asm.asm.to_hex(self.asm.ret))
+        self.asm.add_inst("RH=&" + self.asm.asm.to_hex(self.asm.ret - 1))
+        self.asm.add_inst("PRINT", "~?RH,~?RL")
+
+    def visit_Declaration(self, node):
+        for i, d in enumerate(node.decls):
+            type_name = d.specs[-1].type
+            type_symbol = self.scope.lookup(type_name)
+            if type(d.child) == decl_tree.Identifier:
+                var_name = d.child.identifier.value
+                if self.scope.lookup(var_name, current_scope_only=True):
+                    raise SyntaxError("Duplicate identifier '%s' found" % var_name)
+                self.scope.define(VarSymbol(var_name, type_symbol, self.memstart))
+                self.memstart -= type_symbol.size
+                if node.inits[i] is not None:
+                    self.visit(node.inits[i])
+            elif type(d.child) == decl_tree.Function:
+                func_name = d.child.child.identifier.value
+                if self.scope.lookup(func_name, current_scope_only=True):
+                    raise SyntaxError("Duplicate identifier '%s' found" % func_name)
+                print(d.child.args[0].child)
+                self.scope.define(FunctionSymbol(func_name, type_symbol, d.child.args))
+
+    def visit_Main(self, node):
+        type_symbol = self.scope.lookup(INT)
+        func_name = "main"
+        func_symbol = FunctionSymbol(func_name, type_symbol)
+        self.scope.define(func_symbol)
+        procedure_scope = ScopedSymbolTable(
+            scope_name=func_name,
+            scope_level=self.scope.scope_level + 1,
+            enclosing_scope=self.scope,
+        )
+        self.scope = procedure_scope
+        if ast.Return not in [type(n) for n in node.body.items] and type_symbol.name != VOID:
+            raise SyntaxError("No return in function")
+        self.visit(node.body)
+        self.scope_out.add_scope(procedure_scope)
+        self.scope = self.scope.enclosing_scope
+
+    def visit_Identifier(self, node):
+        var_name = node.identifier.value
+        val = self.scope.lookup(var_name)
+        if val is None:
+            raise NameError(repr(var_name))
+
+    def visit_Compound(self, node):
+        for n in node.items:
+            self.visit(n)
+
+    def visit_ExprStatement(self, node):
+        self.visit(node.expr)
+
+    def visit_Equals(self, node):
+        self.visit(node.right)
+        self.visit(node.left)
+
+    def visit_PlusEquals(self, node):
+        self.visit(node.right)
+        self.visit(node.left)
+
+    def visit_MinusEquals(self, node):
+        self.visit(node.right)
+
+    def visit_StarEquals(self, node):
+        self.visit(node.right)
+        self.visit(node.left)
+        self.visit(node.left)
+
+    def visit_DivEquals(self, node):
+        self.visit(node.right)
+        self.visit(node.left)
+
+    def visit_ModEquals(self, node):
+        self.visit(node.right)
+        self.visit(node.left)
+
+    def visit_MultiExpr(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Number(self, node):
+        pass
+
+    def visit_String(self, node):
+        pass
+
+    def visit_ParenExpr(self, node):
+        self.visit(node.expr)
+
+    def visit_Plus(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Minus(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Mult(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Div(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Mod(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Equality(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Inequality(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_BoolAnd(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_BoolOr(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_PreIncr(self, node):
+        self.visit(node.expr)
+
+    def visit_PostIncr(self, node):
+        self.visit(node.expr)
+
+    def visit_PreDecr(self, node):
+        self.visit(node.expr)
+
+    def visit_PostDecr(self, node):
+        self.visit(node.expr)
+
+    def visit_AddrOf(self, node):
+        self.visit(node.expr)
+
+    def visit_Dref(self, node):
+        self.visit(node.expr)
+
+    def visit_FuncCall(self, node):
+        func_name = node.func.identifier.value
+        func = self.scope.lookup(func_name)
+        if func is None:
+            raise NameError(repr(func_name))
+
+        print(func, node.args)
+        if len(func.params) != len(node.args):
+            raise NameError("Calling " + str(func.name) + " with wrong number of params")
+        for i, a in enumerate(node.args):
+            arg_name = a.identifier.value
+            arg_type = self.scope.lookup(arg_name).type.name
+            if arg_type != func.params[i].specs[-1].type:
+                raise TypeError("Calling " + str(func.name) + " with wrong param type")
+
+    def visit_NoOp(self, node):
+        pass
+
+    def visit_Return(self, node):
+        self.visit(node.right)
+
     def visit_Function(self, node):
         self.asm.add_inst("", label=node.label.value)
         if node.label.value == "main":
